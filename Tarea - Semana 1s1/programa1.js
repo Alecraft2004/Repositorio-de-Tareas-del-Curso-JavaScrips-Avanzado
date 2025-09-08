a0365113ce6720bcf5b3c0bdb8f591eb75abba7e
Alecraft2004 <alecraft2005yt@gmail.com>
2025-09-07 19:08:44 -0500

    Add files via upload

---
+++ Tarea - Semana 1s1/programa1.js
@@ -0,0 +1,24 @@
+// Declaración de 3 variables tipo número
+let edad = 20;        
+let altura = 1.75;   
+let año = 2025;       
+
+// Declaración de 3 variables tipo cadena (texto)
+let nombre = "Alejandro"; 
+let ciudad = "Arequipa";  
+let pais = "Perú";       
+
+// Declaración de 3 variables tipo booleano (verdadero o falso)
+let estudiante = true;   
+let trabaja = false;     
+let mayorEdad = true;   
+
+// Imprimir los valores en la consola
+console.log("Variables tipo número:");
+console.log(edad, altura, año); // Muestra las 3 variables numéricas
+
+console.log("Variables tipo cadena:");
+console.log(nombre, ciudad, pais); // Muestra las 3 variables de texto
+
+console.log("Variables tipo booleano:");
+console.log(estudiante, trabaja, mayorEdad); // Muestra las 3 variables booleanas
